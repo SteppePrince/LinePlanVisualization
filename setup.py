a714c1a9ce7f293ea677d4285d08a15302c2df31
Macroda <133780879+SteppePrince@users.noreply.github.com>
2023-09-15 17:20:09 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-# -*- coding: UTF-8 -*-
 from setuptools import setup, find_packages
 
-with open("README.md", 'r') as ld:
+with open("README.md", 'r', encoding="utf-8") as ld:
     long_description = ld.read()
 
 setup(name="line_plan_visualization",
