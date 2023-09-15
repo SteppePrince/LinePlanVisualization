cc591e6c46e94a49ea6ddf46f8a91554dbaa8e8d
Macroda <133780879+SteppePrince@users.noreply.github.com>
2023-09-15 17:09:46 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as ld:
+    long_description = ld.read()
+
 setup(name="line_plan_visualization",
       version="0.0.3",
       author="Macroda",
       author_email="SteppePrince@outlook.com",
       description="A visualization tool for line plan",
+      long_description=long_description,
       url="https://github.com/SteppePrince/LinePlanVisualization",
       packages=find_packages(),
       install_requires=["matplotlib>=3.7.3"],
